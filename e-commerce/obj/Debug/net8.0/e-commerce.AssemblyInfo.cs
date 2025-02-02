204ac2bba49173386abefee795c3955da4771c6f
Furkan Işınak <sfurkan72@gmail.com>
2025-02-02 12:45:03 +0300

    a variable

--- e-commerce/obj/Debug/net8.0/e-commerce.AssemblyInfo.cs
+++ e-commerce/obj/Debug/net8.0/e-commerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("e-commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc6ac6dfec494e9d9c95db3fa688b7a734ab06f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("e-commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("e-commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
